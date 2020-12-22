10a18afda9b0ce7f0c9cafbd7a8bf2355370b612
Shashank2909 <sajgaonkar007@gmail.com>
2020-12-22 14:52:22 +0530

    update

--- project/detection.py
+++ project/detection.py
@@ -14,37 +14,38 @@ def face_save(name):
     count = 0
     while True:
         ret,img = vid.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
+        faces = face_cascade.detectMultiScale(img, 1.1, 4)
+        cv2.imshow('img',img)
         if faces is None:
             continue
-        elif count<=10:
+        elif count<=50:
             for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                cv2.imshow('cam', img)
-                crop = gray[y:y+h,x:x+w]
-                resize_img = cv2.resize(crop,(128,128))
+                # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                # cv2.imshow('cam', img)
+                crop = img[y:y+h,x:x+w]
+                resize_img = cv2.resize(crop,(256,256))
                 s = '{}-{}.png'.format(name,count)
                 cv2.imwrite(s,resize_img)
                 count+=1
             
                     
 
+
         cv2.imshow('img', img)
        
 
-        if count==10:
+        if count==50:
             break
 
     vid.release()
     cv2.destroyAllWindows()
 
-# i = int(input("Do you want to register a user?...press 1"))
+i = int(input("Do you want to register a user?...press 1"))
 
-# if i==1:
-#     name = input("Enter the name")
-#     face_save(name)
+if i==1:
+    name = input("Enter the name")
+    face_save(name)
 
-face_save('shashank')
+# face_save('s2')
